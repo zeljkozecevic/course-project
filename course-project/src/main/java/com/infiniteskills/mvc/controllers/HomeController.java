7ed07359c66ab1a2bbac04b08b45d1027cdf46a1
RPeko <peko.roganovic@gmail.com>
2016-04-17 21:05:19 +0200

    Model basic 1. commit

--- course-project/src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ course-project/src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -1,13 +1,25 @@
 package com.infiniteskills.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.infiniteskills.mvc.data.entities.Project;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String goHome(){
+	public String goHome(Model model){
+		
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by NASA");
+		
+		model.addAttribute("currentProject", project);
+		
 		return "home";
 	}
+	
 }
