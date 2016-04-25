25bec028b41919933ed49c50785bae14561fea43
nickfit <vujovicnidzo@yahoo.com>
2016-04-25 09:33:09 +0200

    dodao sam komentar

--- course-project/src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ course-project/src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.infiniteskills.mvc.data.entities.Project;
-
+//dodao komentar
 @Controller
 public class HomeController {
 
